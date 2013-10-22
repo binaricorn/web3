298b725e6a00e1905692ef1a2b45a2516686a507
Fei <fei.fei2liu@gmail.com>
2013-10-22 09:47:09 -0400

    Made improvements

--- 03_assignment02/background/etch.js
+++ 03_assignment02/background/etch.js
@@ -1,5 +1,13 @@
 var fs = require( "fs" );
 var url = require( "url" );
+var io = require('socket.io');
+
+// Initialize a REST client in a single line:
+var client = require('twilio')('AC2a00c102356a809f11d7d5db0dbdb77d','c21abe3d71181b2409877728886f9b54');
+
+var myPhone = '+15103585332';
+var twilioPhone = '+17066863423';
+
 
 
 /* Create the server in the port 9000 */
@@ -32,12 +40,34 @@ var http = require( "http" ).createServer(function ( req, res ) {
 ).listen( 9000 );
 
 
-var io = require( "socket.io" ).listen( http );
 
-io.set('log level', 1);
+var socketServer = io.listen( http );
+/* io */
 
-io.sockets.on( "connection", function ( socket ) {
+socketServer.set('log level', 1);
+
+socketServer.sockets.on( "connection", function ( socket ) {
 	// On a new Socket.io connection, load the data provider we want. For now, just Arduino.
 	var $provider = require( './providers/arduino.js' ).init( socket );
+	
+	socket.on("sending name", function(data) {
+		console.log(data);
+		client.sendSms({ 
+			to: myPhone,
+			from: twilioPhone,
+			body: data.name + ' skipped his/her Browsercize at ' + data.skips + ' .'
+		}, function(error, message) {
+			if (!error) {
+				console.log('Success! The SID for this SMS message is:');
+				console.log(message.sid);
+				
+				console.log('Message sent on:');
+				console.log(message.dateCreated);
+			} else {
+				console.log('Oops! There was an error.');
+			}
+		});
+
+	});
 } );
 
--- 03_assignment02/background/frontend/scripts.js
+++ 03_assignment02/background/frontend/scripts.js
@@ -1,29 +1,130 @@
+/* Create empty array and objects */
 var userInfo = [];
+var thisUser = {};
+var data = {};
+var socket = io.connect('http://localhost');
 
+initPage();                              
+pageLoad(false);
 
+function initPage() {
+	/* Hide all the pages except for the first one on load */
+	for(var i = 1; i < 5; i++) {
+		$(".page section").eq(i).css("display","none");
+	}
 
-$('#submit').click(function() {
-	var thisUser = {};
+	$('#submit0').click(function() { 
+		$(".page section").eq(0).fadeOut("slow", function() {
+			$(".page section").eq(1).fadeIn("slow");
+			$("#progress-bar").fadeIn("slow").css("width","25%");
+		});
+	});
 	
-	thisUser.name = $("#name").val();
-	thisUser.pushups = $("input[value='push ups']").is(':checked');
-	thisUser.stretches = $("input[value='stretches']").is(':checked');		
-	thisUser.time = $("#time").val();
+	/* First page: Get username */
+	$('#submit1').click(function() {
+		thisUser.name = $("#name").val();
 		
-	userInfo.push(thisUser);
-	storeUser(userInfo);
-});
-
-
-
-function storeUser(userInfo) {
-	userInfo = JSON.stringify(userInfo);
-	localStorage.userInfo = userInfo;
-
+		$(".page section").eq(1).fadeOut("slow", function() {
+			$(".page section").eq(2).fadeIn("slow");
+			$("#progress-bar").css("width","50%");
+		});
+		
+	});
 }
 
+		
+function pageLoad(editing) {
+		
+	/* Initialize time variable from HTML*/
+	var timeVal = parseInt($("#time-val").text());
+	
+	/* Do math on time variable and display on HTML page */
+	if (localStorage.userInfo) {
+			userInfo = localStorage.userInfo;
+			userInfo = JSON.parse(userInfo);
+			var currentEdit = userInfo.length-1;
+			$('#time-val').text(userInfo[currentEdit].time);
+			
+			$("#decrease-time").click(function() {
+				timeVal -= 5;
+				$("#time-val").text(timeVal);
+			});
+			
+			$("#increase-time").click(function() {
+				timeVal = +timeVal + 5;
+				$("#time-val").text(timeVal);	
+			});
+			
+		} else {
+			$("#decrease-time").click(function() {
+				timeVal -= 5;	
+				$("#time-val").text(timeVal);	
+			});
+			
+			$("#increase-time").click(function() {
+				timeVal = +timeVal + 5;
+				$("#time-val").text(timeVal);	
+			});
+	}
+	
+	
+	/* Second page: Get exercise selection */
+	$('#submit2').click(function() {
+		thisUser.pushups = $("input[value='push ups']").is(':checked');
+		thisUser.stretches = $("input[value='stretches']").is(':checked');	
+		$(".page section").eq(2).fadeOut("slow", function() {
+			$(".page section").eq(3).fadeIn("slow");
+			$("#progress-bar").css("width","75%");
+		});
+		
+	});
+	
+	if(editing) {
+		/* Third page: Get timer selection */
+		$('#submit3').click(function() {
+			thisUser.time = $("#time-val").text();			
+			userInfo.push(thisUser);
+			storeUser(userInfo);
+		});
+		
+	} else {
+		/* Third page: Get timer selection */
+		$('#submit3').click(function() {
+			thisUser.time = $("#time-val").text();
+			$(".page section").eq(3).fadeOut("slow", function() {
+				$(".page section").eq(4).fadeIn("slow");
+				$("#progress-bar").css("width","100%");
+			});
+		});
+		
+		
+		/* Fourth page: Get timer selection */
+		$('#submit4').click(function() {
+			userInfo.push(thisUser);
+			storeUser(userInfo);
+			setTimeout(function() {
+				location.reload();
+			}, 500)
+		
+		});
+	}
+}	
+
+function storeUser(userInfo) {
+	/* localStorage.deleteItem('userInfo'); */
+	if(localStorage.getItem('userInfo') === null) {
+		localStorage.setItem('userInfo', JSON.stringify(userInfo));
+	} else {
+		localStorage.removeItem('userInfo');
+		setTimeout(function() {
+		localStorage.setItem('userInfo', JSON.stringify(userInfo));
+		}, 500);
+	}
+}
 
 checkForUserName();
+
+
 
 function checkForUserName() {	
 	if (localStorage.userInfo) {
@@ -33,10 +134,130 @@ function checkForUserName() {
 	}
 }
 
+
 function haveName() {
-	$('body').append("hello " + userInfo[0].name);
+	goGoGo();
 }
 
+			
+			function hideGo() {
+				$('#overlay').fadeOut("fast");
+			}
+			
+			function goGoGo() {
+			/* Initiating values */	
+				var num = 10;			
+				var step1, step2, step3 = false;
+				var skips = {};
+				var skipsData = [];
+				
+			
+				$("body").append('<section id="overlay"><article id="cover"><div id="warning"><div id="warning-content"><div id="warning-text"><p>I\'m sorry '+ userInfo[0].name +' but you must complete ' +num+ ' Browsercizes to continue using this site.</p><p><a href="#" id="start" class="goBtn">Start</a>&nbsp;&nbsp;<a href="#" id="skip" class="goBtn">Skip</a>&nbsp;&nbsp;</p><div id="edit"></div></div><div class="clear"></div></div></div><div id="cover-content"></div></article></section>');
+
+				var winHeight = $(window).height();
+				var coverHeight = $("#cover").height();
+				var topPlace = winHeight - coverHeight - (coverHeight/3);
+			
+			
+				$("#cover-content").css("margin-top", topPlace);
+			
+				
+				$('#skip').click(function() {
+					if (localStorage.userInfo) {
+						userInfo = localStorage.userInfo;
+						userInfo = JSON.parse(userInfo);
+						var currentEdit = userInfo.length-1;
+						
+						skips.skipTime = new Date();
+						skipsData.push(skips);
+						skipsData = JSON.stringify(skipsData);
+						localStorage.skipsData = skipsData;
+						
+						/* Read-out of how many and when skips take place in the storage */
+						if (localStorage.skipsData) {
+						skipsData = localStorage.skipsData;
+						skipsData = JSON.parse(skipsData);
+						for(i = 0; i < skipsData.length; i++) {
+							console.log(i + ": " + skipsData[i].skipTime);	
+							
+						} 
+						data.skips = skipsData[skipsData.length-1].skipTime;
+						
+					}
+						data.name = userInfo[currentEdit].name;
+						socket.emit('sending name', data);
+						console.log(data);
+						
+					}
+					
+					setTimeout(function() {
+						window.open('','_self',''); 
+						window.close();
+					}, 500);
+						
+					/*
+					hideGo(); 	
+					*/
+					
+					
+				});
+				
+				$('#edit').mouseover(function() {
+					$(this).css({"opacity":"0.5","transition":"opacity 0.3s ease-in-out"});
+				}).mouseout(function() {
+					$(this).css({"opacity":"1","transition":"opacity 0.3s ease-in-out"});
+				}).click(function() {
+					hideGo();
+					pageLoad(true);
+					
+					
+					$(".page section").eq(0).css("display","none");
+					$(".page section").eq(1).css("display","block");
+						
+					
+					if (localStorage.userInfo) {
+						userInfo = localStorage.userInfo;
+						userInfo = JSON.parse(userInfo);
+						console.log(userInfo.length);
+						
+						/* Getting the new editted values */
+						var currentEdit = userInfo.length-1;
+						$('input#name').val(userInfo[currentEdit].name);
+
+						
+						if(userInfo[currentEdit].pushups == true) {
+							$("input[value='push ups']").prop('checked', true);
+						}
+						if(userInfo[currentEdit].stretches == true) {
+							$("input[value='stretches']").prop('checked', true);
+						}
+						
+					}
+				});
+				
+				
+
+				$("#warning p a#start").click(function() {
+					$("#warning-img").fadeOut(300, function() {
+						$(this).remove();
+					});
+				
+					
+					$("#warning-text").fadeOut(300, function() {
+						$(this).css("width","100%").text("1. Make sure both your feet are placed correctly on the mat.").fadeIn();
+					});
+					
+					//now hold this for __ seconds
+					//score-card grays out if the feet are not placed on the mat properly
+					
+					$("#cover-content").fadeOut(300, function() {
+						$("#cover").css("margin-top", "0px");
+						$(this).append("<div id='touchCounter'><div class='stretch-count'></div></div><div id='leftFoot'></div><div id='rightFoot'></div>").css({"position":"relative", "margin-top":"150px", "background-image": "none", "width":"382px", "height":"580px", "border":"5px solid white"}).fadeIn();
+					});
+					
+					});
+				};
+					
 
 jQuery( document ).ready( function () {
 		
@@ -94,9 +315,7 @@ jQuery( document ).ready( function () {
 					if (stretches <= 0) {
 						$('#overlay').hide("fast");
 					}
-				}
-				
-				
+				}	
 			};
 			
 			
@@ -110,8 +329,6 @@ jQuery( document ).ready( function () {
 				setInterval(startCount(), 1000/30);
 			}
 			
-			
-		
 		
 		} );
 
